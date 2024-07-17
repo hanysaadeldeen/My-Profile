bd99633b12727a8013fb3e1b0fe09e5da8ebbc5c
HanyMohamed <hsaad9231@gmail.com>
2024-07-17 10:28:19 +0300

    catch the error with try-catch and add favicon

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,5 +1,5 @@
 "use server";
-import { validateString } from "@/lib/utiles";
+import { getErrorMessage, validateString } from "@/lib/utiles";
 
 import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -7,6 +7,9 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const getData = async (formdata: FormData) => {
   const senderEmail = formdata.get("senderEmail");
   const message = formdata.get("description");
+
+  console.log(senderEmail);
+  console.log(message);
 
   if (!validateString(senderEmail)) {
     return {
@@ -19,11 +22,17 @@ export const getData = async (formdata: FormData) => {
     };
   }
 
-  resend.emails.send({
-    from: "Contact Form <onboarding@resend.dev>",
-    reply_to: senderEmail as string,
-    to: "hanymohamedsaadeldean@gmail.com",
-    subject: "Message from contact form",
-    text: message as string,
-  });
+  try {
+    resend.emails.send({
+      from: "Contact Form <onboarding@resend.com>",
+      reply_to: senderEmail as string,
+      to: "hanymohamedsaadeldean@gmail.com",
+      subject: "Message from contact form",
+      text: message as string,
+    });
+  } catch (error: unknown) {
+    return {
+      error: getErrorMessage(error),
+    };
+  }
 };
